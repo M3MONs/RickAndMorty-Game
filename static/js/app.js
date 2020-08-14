a5f223c7b1242023f4a8b627755b25bbca180a84
M3MONs <alan.skrrrz@gmail.com>
2020-08-14 16:49:23 +0200

    add combat system

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,70 @@
 import API from "./api.js";
 import Character from "./character.js";
-import * as say from './combatsystem.js';
+import * as say from "./combatsystem.js";
 const api = new API();
 
 let currentCaracter = 1;
+let enemyCharacterId;
 const $loadNext = document.querySelector("#load-next");
 const $loadPrevious = document.querySelector("#load-previous");
 const $loadGame = document.querySelector("#load-game");
 
-
 //Next Character
 $loadNext.addEventListener("click", async () => {
-  if (currentCaracter <= 5) {
-    if (currentCaracter === 5) {
-      currentCaracter = 0;
-    }
-    var characterData = await api.getCharacter(++currentCaracter);
-    new Character(characterData);
-    Stats(currentCaracter);
-  }
+	if (currentCaracter <= 5) {
+		if (currentCaracter === 5) {
+			currentCaracter = 0;
+		}
+		var characterData = await api.getCharacter(++currentCaracter);
+		new Character(characterData);
+		Stats(currentCaracter);
+	}
 });
 
 //Prevoius Character
 $loadPrevious.addEventListener("click", async () => {
-  if (currentCaracter > 0) {
-    //loop for card set next card id=1 if we click next on card id=5
-    if (currentCaracter === 1) {
-      currentCaracter = 6;
-    }
-    var characterData = await api.getCharacter(--currentCaracter);
-    new Character(characterData);
-    Stats(currentCaracter);
-  }
+	if (currentCaracter > 0) {
+		//loop for card set next card id=1 if we click next on card id=5
+		if (currentCaracter === 1) {
+			currentCaracter = 6;
+		}
+		var characterData = await api.getCharacter(--currentCaracter);
+		new Character(characterData);
+		Stats(currentCaracter);
+	}
 });
 
+function createEnemy() {
+	enemyCharacterId = Math.floor(Math.random() * 100) + 50;
+	initApp(enemyCharacterId);
+	Stats(enemyCharacterId);
+}
 //Load Enemy card and game
 $loadGame.addEventListener("click", async () => {
-  //create enemy card
-  var enemyCharacterId = Math.floor(Math.random() * 100) + 101;
-  initApp(enemyCharacterId);
-  var enemyCharacterCard = document.querySelector(
-    "#enemy-character-card-container"
-  );
-  enemyCharacterCard.classList.remove("d-none");
+	//create enemy card
+	createEnemy();
+	var editCard = document.querySelectorAll(".flip-card-container");
+	editCard.forEach((card) => {
+		card.classList.add("flip-card-container-fight");
+  });
+  
+	const displayFight = document.querySelectorAll(".fight-display");
+  displayFight.forEach(element => {
+    element.classList.remove("d-none");
+  });
 
-  //exclusion of unnecessary elements
-  var playButton = document.querySelector("#load-game");
-  playButton.classList.add("d-none");
-  var scrollButtons = document.querySelector("#load-previous");
-  scrollButtons.classList.add("d-none");
-  var scrollButtons1 = document.querySelector("#load-next");
-  scrollButtons1.classList.add("d-none");
+  const undisplayFight = document.querySelectorAll(".not-fight-display");
+  undisplayFight.forEach(element => {
+    element.classList.add("d-none");
+  });
 });
-
 
 //create first main Character card
 async function initApp(initCharacterId) {
-  const characterData = await api.getCharacter(initCharacterId);
-  console.log(characterData);
-  new Character(characterData);
-  Stats(currentCaracter);
+	const characterData = await api.getCharacter(initCharacterId);
+	console.log(characterData);
+	new Character(characterData);
+	Stats(currentCaracter);
 }
 
 initApp(currentCaracter);
--- static/js/combatsystem.js
+++ static/js/combatsystem.js
@@ -1,106 +1,191 @@
 function Stats(id) {
-    this.id =id;
-  let health;
-  let atack;
-  let defense;
-  $renderStat = document.querySelector("#character-stat-back");
-  $renderEnemyStat = document.querySelector("#enemy-character-stat");
-  switch (id) {
-    case 1:
-        RickSanchez();
-      break;
-    case 2:
-      MortySmith();
-      break;
-    case 3:
-        SummerSmith();
-      break;
-    case 4:
-        BethSmith();
-      break;
-    case 5:
-        JerrySmith();
-      break;
-    default:
-      $renderStat.innerHTML = "in progress";
-      break;
-  }
+	this.id = id;
+	//variables to set stats
+	let health;
+	let atack;
+	let defense;
 
-  function RickSanchez() {
-    health = 100;
-    atack = 25;
-    defense = 15;
-    renderStats(health, atack, defense);
-    renderEnemyStats(health, atack, defense);
-  }
+	let enemyHealth;
+	let enemyAtack;
+	let enemyDefense;
 
-  function MortySmith() {
-    health = 75;
-    atack = 17;
-    defense = 20;
-    renderStats(health, atack, defense);
-    renderEnemyStats(health, atack, defense);
-  }
+	//Select docuument to render stats
 
-  function SummerSmith() {
-    health = 90;
-    atack = 17;
-    defense = 20;
-    renderStats(health, atack, defense);
-    renderEnemyStats(health, atack, defense);
-  }
+	const renderHealth = document.querySelectorAll(".char-h");
+	const renderAtack = document.querySelectorAll(".char-a");
+	const renderDefense = document.querySelectorAll(".char-d");
 
-  function BethSmith() {
-    health = 80;
-    atack = 17;
-    defense = 20;
-    renderStats(health, atack, defense);
-    renderEnemyStats(health, atack, defense);
-  }
+	const renderEnemyHealth = document.querySelector("#echar-h");
+	const renderEnemyAtack = document.querySelector("#echar-a");
+	const renderEnemyDefense = document.querySelector("#echar-d");
 
-  function JerrySmith() {
-    health = 50;
-    atack = 12;
-    defense = 12;
-    renderStats(health, atack, defense);
-    renderEnemyStats(health, atack, defense);
-  }
+	class Character {
+		constructor(health, atack, defense) {
+			this.health = health;
+			this.atack = atack;
+			this.defense = defense;
+		}
+		rendStat() {
+			setStats(this.health, this.atack, this.defense);
+		}
+	}
+	// Stats Characters (health, atack, defense)
+	const RickSanchez = new Character(100, 25, 15);
+	const MortySmith = new Character(75, 17, 20);
+	const SummerSmith = new Character(90, 20, 15);
+	const BethSmith = new Character(80, 23, 18);
+	const JerrySmith = new Character(50, 12, 12);
 
-  function renderStats(health, atack, defense) {
-    $renderStat.innerHTML = `
-    <h2><i class="fa fa-heart stat-icon" aria-hidden="true"></i> ${health}</h2>
-    <h2><img src="static/imgs/sword.svg" alt="" class="stat-icon"> ${atack}</h2>
-    <h2><img src="static/imgs/shield.svg" alt="" class="stat-icon"> ${defense}</h2>
+	//generate character and enemy character stats
+	switch (id) {
+		case 1:
+			RickSanchez.rendStat();
+			break;
+		case 2:
+			MortySmith.rendStat();
+			break;
+		case 3:
+			SummerSmith.rendStat();
+			break;
+		case 4:
+			BethSmith.rendStat();
+			break;
+		case 5:
+			JerrySmith.rendStat();
+			break;
+		default:
+			setEnemyStats();
+			break;
+	}
+	function setStats(s1, s2, s3) {
+		health = s1;
+		atack = s2;
+		defense = s3;
+		renderStats(atack, defense);
+		renderHealths(health, id);
+	}
 
-    `;
-  }
+	// Render Character and Enemy Character Stats
+	function renderStats(cAtack, cDefense) {
+		if (id <= 5) {
+			renderAtack.forEach((element) => {
+				element.innerHTML = ` ${cAtack}`;
+			});
+			renderDefense.forEach((element) => {
+				element.innerHTML = ` ${cDefense} `;
+			});
+		} else {
+			renderEnemyAtack.innerHTML = ` ${cAtack}`;
+			renderEnemyDefense.innerHTML = ` ${cDefense}`;
+		}
+	}
+	function renderHealths(cHealth, check) {
+		if (check <= 5) {
+			renderHealth.forEach((element) => {
+				element.innerHTML = ` ${cHealth}`;
+			});
+		} else {
+			renderEnemyHealth.innerHTML = ` ${cHealth}`;
+		}
+	}
 
-  function renderEnemyStats(health, atack, defence) {
+	function getRandomEnemyStat(charStat) {
+		min = Math.ceil(charStat - 9);
+		max = Math.floor(charStat + 8);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
-    let enemyHealth = getRandomEnemyStat(id,health);
-    let enemyAtack = getRandomEnemyStat(id,atack);
-    let enemyDefense = getRandomEnemyStat(id,defense);
+	function setEnemyStats() {
+		enemyHealth = getRandomEnemyStat(Number(renderHealth[1].innerHTML.valueOf()));
+		enemyAtack = getRandomEnemyStat(Number(renderAtack[1].innerHTML.valueOf()));
+		enemyDefense = getRandomEnemyStat(Number(renderDefense[1].innerHTML.valueOf()));
+		renderStats(enemyAtack, enemyDefense);
+		renderHealths(enemyHealth, 6);
+	}
 
-    $renderEnemyStat.innerHTML = `
-    <h2><i class="fa fa-heart stat-icon" aria-hidden="true"></i> ${enemyHealth}</h2>
-    <h2><img src="static/imgs/sword.svg" alt="" class="stat-icon"> ${enemyAtack}</h2>
-    <h2><img src="static/imgs/shield.svg" alt="" class="stat-icon"> ${enemyDefense}</h2>
+	//Atack
+	document.querySelector("#atack-btn").onclick = function () {
+		characterAtack();
+	};
 
-    `;
+	function characterAtack() {
+		var charHealth = Number(renderHealth[1].innerHTML.valueOf());
+		var charAtack = Number(renderAtack[1].innerHTML.valueOf());
 
-  }
+		var eCharHp = Number(renderEnemyHealth.innerHTML.valueOf());
+		var eCharDefense = Number(renderEnemyDefense.innerHTML.valueOf());
+		if (charHealth > 0) {
+			eCharHp = atackAlgorithm(eCharHp, charAtack, eCharDefense) - 5;
+			renderHealths(eCharHp, 6);
+			//must chceck enemy hp
+			enemyCombatFunction();
+		}
+		checkResult();
+	}
 
-  function getRandomEnemyStat(id,charStat) {
-      //hardest statistics for players who play Jerry
-      if(id===5){
-        min = Math.ceil(charStat-5);
-        max = Math.floor(charStat+11);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }else{
-    min = Math.ceil(charStat-12);
-    max = Math.floor(charStat+12);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-}
-  console.log(health)
+	//Regen
+	document.querySelector("#regen-btn").onclick = function () {
+		characterRegen();
+	};
+
+	function characterRegen() {
+		var charHealth = Number(renderHealth[1].innerHTML.valueOf());
+		let eCharHp = Number(renderEnemyHealth.innerHTML.valueOf());
+		if (charHealth < health - 20 && eCharHp > 0) {
+			charHealth += randomNumberFight() + 15;
+			renderHealths(charHealth, 1);
+		}
+
+		var counterMove = randomNumberFight();
+		if (counterMove < -1) {
+			enemyCombatFunction();
+		}
+	}
+
+	function enemyCombatFunction() {
+		var enemyAction = randomNumberFight();
+
+		var charHealth = Number(renderHealth[1].innerHTML.valueOf());
+		var charDefense = Number(renderDefense[1].innerHTML.valueOf());
+
+		var eCharAtack = Number(renderEnemyAtack.innerHTML.valueOf());
+		var eCharHp = Number(renderEnemyHealth.innerHTML.valueOf());
+		if (charHealth > 0) {
+			if (enemyAction >= -3) {
+				charHealth = atackAlgorithm(charHealth, eCharAtack, charDefense) - 7;
+				//must chceck char hp
+				renderHealths(charHealth, 1);
+			} else {
+				eCharHp += randomNumberFight() - 5;
+				renderHealths(eCharHp, 6);
+			}
+		}
+		checkResult();
+	}
+
+	function atackAlgorithm(h, a, d) {
+		h -= Math.abs(Math.round((a - d) * 1.1) + randomNumberFight());
+		return h;
+	}
+
+	function randomNumberFight() {
+		return Math.floor(Math.random() * 9 - 4);
+	}
+
+	function checkResult() {
+		var charHealth = Number(renderHealth[1].innerHTML.valueOf());
+		var eCharHp = Number(renderEnemyHealth.innerHTML.valueOf());
+
+		const win = document.querySelector("#enemy-stats");
+		const lose = document.querySelector("#front-stats");
+		if(eCharHp <= 0 && charHealth <= 0){
+			win.innerHTML = `<h1 class="m-auto">Draft~!</h1>`;
+			lose.innerHTML = `<h1 class="m-auto">Draft~!</h1>`
+		}else if (eCharHp <= 0) {
+			win.innerHTML = `<h1 class="m-auto">You Win~!</h1>`;
+		}else if (charHealth <= 0) {
+			console.log("działa?");
+			lose.innerHTML = `<h1 class="m-auto">You Lose~!</h1>`;
+		}
+	}
 }
